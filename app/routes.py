865182328c7e75c631bd77e6bddd3c8ac76e2ed9
rafael1717y <rafaelufjf@gmail.com>
2022-05-08 18:37:07 -0300

    alterações na arquitetura

---
+++ app/routes.py
@@ -0,0 +1,70 @@
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
+
+@app.route("/")  # users
+def index():
+    #users = Users.query.all()   # select * from users;
+    #return render_template("users.html", users=users)
+    return render_template("base.html")
+
+
+#@login_required  # um usuário logado pode ver sua simulação
+@app.route('/simulations')
+def simulations():
+    # retrun render_template(url_for('simulations', title='Simulações', simualations=simulations))
+    return render_template('simulations.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return render_template('simulations.html')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+
+# TODO: enviar resultados simulações calculados em uma rota para o template
+
+
