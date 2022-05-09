502becc73e48462ff39b5276115b3a4c77b7f044
rafael1717y <rafaelufjf@gmail.com>
2022-05-09 07:17:58 -0300

    tratamento erros

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from ensurepip import bootstrap
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -5,14 +6,21 @@ from flask_login import LoginManager
 from config import Config
 
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
+
+# Registro das extensões
+# -----------------------
+bootstrap = bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+# Depois da instância da aplicação criada: 
+# ----------------------------------------
+from app import routes, models, errors
--- app/forms.py
+++ app/forms.py
@@ -4,28 +4,30 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Nome', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Lembrar-me')
+    submit = SubmitField('Entrar')
 
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Nome', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField('Repita a senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Cadastrar')
+
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Please use a different username.')
+            raise ValidationError('Por favor, use um nome diferente.')
 
+    
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError('Por favor, use um email diferente.')
